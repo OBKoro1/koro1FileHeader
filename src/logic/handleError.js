9a3bcaf1fd7ea3f4627e23ee5cd69b01572a9527
OBKoro1 <1677593011@qq.com>
2020-02-13 15:51:50 +0800

    修复通知报错

--- src/logic/handleError.js
+++ src/logic/handleError.js
@@ -2,8 +2,8 @@
  * Author: OBKoro1
  * Github: https://github.com/OBKoro1
  * Date: 2019-09-04 20:36:56
- * @LastEditors: OBKoro1
- * @LastEditTime: 2019-12-09 10:51:21
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2020-02-13 12:42:22
  * Description: 报错拦截
  */
 
@@ -11,7 +11,7 @@ const vscode = require('vscode');
 
 const showErrorMessage = (e)=>{
     const config = vscode.workspace.getConfiguration('fileheader'); // 配置项默认值
-    if(!config.configObj.filePathColon) return // 关闭报错
+    if(!config.configObj.showErrorMessage) return // 关闭报错
     if(typeof e !== 'string'){
         e = e.message
     }
@@ -20,7 +20,7 @@ const showErrorMessage = (e)=>{
 
 process.on('uncaughtException', function (e) {
     const config = vscode.workspace.getConfiguration('fileheader'); // 配置项默认值
-    if(!config.configObj.filePathColon) return // 关闭报错
+    if(!config.configObj.showErrorMessage) return // 关闭报错
     vscode.window.showErrorMessage('错误信息:', JSON.stringify(e))
 });
 
