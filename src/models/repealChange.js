03b285d562a9b49752149d3e3de5b895d56b3fde
OBKoro1 <1677593011@qq.com>
2020-05-07 14:11:51 +0800

    获取文件后缀匹配特殊文件以及匹配黑名单文件

--- src/models/repealChange.js
+++ src/models/repealChange.js
@@ -8,80 +8,86 @@
  * https://github.com/OBKoro1
  */
 
-const util = require('../utile/util');
-const execSync = require('child_process').execSync;
-const fs = require('fs');
-const languageDiff = require('../languageOutPut/languageDifferent');
+const util = require('../utile/util')
+const execSync = require('child_process').execSync
+const fs = require('fs')
+const languageDiff = require('../languageOutPut/languageDifferent')
 
 class RepealChange {
   /**
    * @param {Boolean} CheckFileChange 是否进行diff检查
    */
   constructor(CheckFileChange) {
-    this.resetFile = false; // 依据这个值来判断文件是否重置
+    this.resetFile = false // 依据这个值来判断文件是否重置
     if (CheckFileChange) {
-      this.init();
+      this.init()
     }
   }
   init() {
-    const filePathObj = util.getFileRelativeSite();
-    this.itemPath = filePathObj.itemPath;
+    const filePathObj = util.getFileRelativeSite()
+    this.itemPath = filePathObj.itemPath
     if (this.hasGit()) {
-      this.fsPath = filePathObj.fsPath;
-      this.getDiff();
+      this.fsPath = filePathObj.fsPath
+      this.getDiff()
     }
   }
   hasGit() {
-    const url = `${this.itemPath}/.git`; // 文件路径
+    const url = `${this.itemPath}/.git` // 文件路径
     try {
-      let isDirectory = fs.statSync(url).isDirectory(); // 判断是否为文件夹 返回布尔值
+      let isDirectory = fs.statSync(url).isDirectory() // 判断是否为文件夹 返回布尔值
       if (isDirectory) {
-        return true;
+        return true
       } else {
-        return false;
+        return false
       }
     } catch (err) {
-      return false; // 路径不存在的情况
+      return false // 路径不存在的情况
     }
   }
   getDiff() {
     const diffStr = this.myExecSync(
       `cd ${this.itemPath} && git diff ${this.fsPath}`
-    );
-    const fileIsChange = this.checkDiff(diffStr);
+    )
+    const fileIsChange = this.checkDiff(diffStr)
     if (!fileIsChange) {
       // 文件只改变了时间和作者 放弃修改
-      this.myExecSync(`cd ${this.itemPath} && git checkout -- ${this.fsPath}`);
-      this.resetFile = true;
+      this.myExecSync(`cd ${this.itemPath} && git checkout -- ${this.fsPath}`)
+      this.resetFile = true
     }
   }
   checkDiff(diffString) {
     // 切割diff字符串
-    let splitReg = /@@ [+-].*@@/m;
-    let splitArr = diffString.split(splitReg);
+    let splitReg = /@@ [+-].*@@/m
+    let splitArr = diffString.split(splitReg)
     if (splitArr.length !== 2) {
-      return true; // 应该只有一个变更 只能切出一个@@
+      return true // 应该只有一个变更 只能切出一个@@
     }
-    diffString = splitArr[1];
-    let regString = /\r\n|\r|\n/; // 切割换行字符串 转义\\
-    let stringArr = diffString.split(regString); // 切割换行字符串
+    diffString = splitArr[1]
+    let regString = /\r\n|\r|\n/ // 切割换行字符串 转义\\
+    let stringArr = diffString.split(regString) // 切割换行字符串
     // 检测每行字符串
-    let reg = /^[-+]/; // 必须以 - 或者 + 开头
+    let reg = /^[-+]/ // 必须以 - 或者 + 开头
     // 获取文件头
-    languageDiff.tplJudge.prototype.initConfig();
-    let lastEditorName = languageDiff.tplJudge.prototype.LastEditorsName;
-    let lastTimeName = languageDiff.tplJudge.prototype.LastEditTimeName;
+    languageDiff.tplJudge.prototype.initConfig()
+    let lastEditorName = languageDiff.tplJudge.prototype.LastEditorsName
+    let lastTimeName = languageDiff.tplJudge.prototype.LastEditTimeName
+    let filePathName = languageDiff.tplJudge.prototype.filePathName
     for (let item of stringArr.values()) {
       // 检测有变更的字符串
       if (item.match(reg) !== null) {
-        if (item.indexOf(`${lastEditorName}`) !== -1) {
-        } else if (item.indexOf(`${lastTimeName}`) !== -1) {
+        const nameReg = new RegExp(`[\\s\\W]${lastEditorName}[\\W\\s]`, 'g')
+        const timeReg = new RegExp(`[\\s\\W]${lastTimeName}[\\W\\s]`, 'g')
+        const filePathReg = new RegExp(`[\\s\\W]${filePathName}[\\W\\s]`, 'g')
+        // 检测是否只有这三个变更
+        if (nameReg.test(item)) {
+        } else if (timeReg.test(item)) {
+        } else if (filePathReg.test(item)) {
         } else {
-          return true; // 有其他变更 不恢复文件
+          return true // 有其他变更 不恢复文件
         }
       }
     }
-    return false; // 恢复文件
+    return false // 恢复文件
   }
   myExecSync(cmd) {
     // 除了该方法直到子进程完全关闭后才返回 执行完毕 返回
@@ -92,13 +98,13 @@ class RepealChange {
         maxBuffer: 200 * 1024,
         killSignal: 'SIGTERM',
         cwd: undefined,
-        env: undefined
-      });
-      return res;
+        env: undefined,
+      })
+      return res
     } catch (err) {
-      console.log('执行命令出错:' + cmd);
+      console.log('执行命令出错:' + cmd)
     }
   }
 }
 
-module.exports = RepealChange;
+module.exports = RepealChange
--- src/utile/util.js
+++ src/utile/util.js
@@ -6,9 +6,9 @@
  * LastEditTime : 2020-02-13 12:32:24
  */
 
-const vscode = require('vscode');
-const moment = require('moment');
-const path = require('path');
+const vscode = require('vscode')
+const moment = require('moment')
+const path = require('path')
 
 /**
  * @description: 节流函数 单位时间内有事件被多次触发则，只生效一次
@@ -18,29 +18,50 @@ const path = require('path');
  * @return: 上次触发time
  */
 const throttle = (fn, gapTime, _lastTime = null) => {
-  return function() {
-    let _nowTime = Date.now();
+  return function () {
+    let _nowTime = Date.now()
     if (_nowTime - _lastTime > gapTime || !_lastTime) {
       // !_lastTime 第一次进入
-      fn(); // 当前时间- 上次执行的时间 超过 给定时间间隔 就执行回调
-      _lastTime = _nowTime; // 触发后，上次执行时间赋值为当前时间
-      return _lastTime;
+      fn() // 当前时间- 上次执行的时间 超过 给定时间间隔 就执行回调
+      _lastTime = _nowTime // 触发后，上次执行时间赋值为当前时间
+      return _lastTime
     }
-    return _lastTime;
-  };
-};
+    return _lastTime
+  }
+}
 
 /**
  * @description: 切割文件路径 获取文件后缀
  * @param {String} fsPath 文件路径
  * @return: 文件后缀
  */
-const fsPathFn = fsPath => {
-  const pathArr = fsPath.split('/');
-  const fileName = pathArr[pathArr.length - 1]; // 取/最后一位
-  const fileNameArr = fileName.split('.');
-  return fileNameArr[fileNameArr.length - 1]; // 取.最后一位
-};
+const fsPathFn = (fsPath) => {
+  const pathArr = fsPath.split('/')
+  const fileName = pathArr[pathArr.length - 1] // 获取文件名
+  return getFsNameEnd(fileName)// 获取文件后缀 
+}
+
+/** 
+ * description: 获取文件后缀，匹配特殊文件
+ * param {string} fileName
+ * Date: 2020-04-27 16:31:35
+ * return: 特殊文件的后缀/文件后缀
+ */
+const getFsNameEnd = (fileName) => {
+  const config = vscode.workspace.getConfiguration('fileheader') // 配置项
+  const language = config.configObj.language // 自定义语言项
+  const fileNameArr = fileName.split('.')
+  // 多个.的文件名 从多到少匹配自定义注释
+  for (const index of fileNameArr.keys()) {
+    const newArr = JSON.parse(JSON.stringify(fileNameArr))
+    newArr.splice(0, index + 1)
+    let endStr = newArr.join('.')
+    if (language[endStr]) {
+      return endStr
+    }
+  }
+  return fileNameArr[fileNameArr.length - 1] // 取.最后一位
+}
 
 /**
  * 以哪种形式生成注释
@@ -49,164 +70,176 @@ const fsPathFn = fsPath => {
  * 2. 插件自带的语言符号
  * 3. 无法识别的语言 默认的注释符号
  */
-const fileEndMatch = fileEnd => {
-  const config = vscode.workspace.getConfiguration('fileheader'); // 配置项
-  const editor = vscode.editor || vscode.window.activeTextEditor; // 选中文件
-  let fsName = fsPathFn(editor._documentData._uri.fsPath); // 文件后缀
+const fileEndMatch = (fileEnd) => {
+  const config = vscode.workspace.getConfiguration('fileheader') // 配置项
+  const editor = vscode.editor || vscode.window.activeTextEditor // 选中文件
+  let fsName = fsPathFn(editor._documentData._uri.fsPath) // 文件后缀
   //  匹配用户自定义语言
   let isMatch = userLanguageFn(
     config,
     fileEnd,
     fsName,
     editor._documentData._uri.fsPath
-  );
+  )
   if (isMatch) {
-    return isMatch; // 匹配到了 返回对象
+    return isMatch // 匹配到了 返回对象
   }
   const obj = {
-    '/^java$|^javascript$|^typescript$|^go$|^cpp$|^php$|^c$/': 'javascript',
+    '/^java$|^javascript$|^typescript$|^go$|^cpp$|^php$|^dart$|^c$/': 'javascript',
     '/^python$/': 'python',
     '/^vb$/': 'vb',
     '/^vue$|^html$|^markdown$/': 'html',
-    '/^shellscript$/': 'shellscript'
-  };
+    '/^shellscript$/': 'shellscript',
+  }
   // 匹配插件支持的注释符号
   for (let key in obj) {
     // 正则匹配
-    const reg = eval(key);
-    const isMatch = reg.test(fileEnd);
+    const reg = eval(key)
+    const isMatch = reg.test(fileEnd)
     if (isMatch) {
-      return obj[key];
+      return obj[key]
     }
   }
   // 默认注释符号
-  return '匹配不到_默认注释';
-};
+  return '匹配不到_默认注释'
+}
 
 // 是否使用用户配置
 function userLanguageFn(config, fileEnd, fsName, fsPath) {
   // 特殊文件
-  const language = config.configObj.language; // 自定义语言项
-  let isSpecial = specialLanguageFn(fsPath, config);
+  const language = config.configObj.language // 自定义语言项
+  let isSpecial = specialLanguageFn(fsPath, config)
   if (isSpecial) {
     return {
       fileEnd: isSpecial,
-      userLanguage: true // 使用用户的配置
-    };
+      userLanguage: true, // 使用用户的配置
+    }
   }
 
   // 检查用户是否设自定义语言 匹配语言
   if (language[fileEnd]) {
     return {
       fileEnd,
-      userLanguage: true // 使用用户的配置
-    };
+      userLanguage: true, // 使用用户的配置
+    }
   } else if (language[fsName]) {
     // 语言没有匹配到 单独匹配一下文件后缀
     return {
       fileEnd: fsName,
-      userLanguage: true
-    };
+      userLanguage: true,
+    }
   }
   for (let key in language) {
     if (key.indexOf('/') !== -1) {
-      const keyArr = key.split('/');
+      const keyArr = key.split('/')
       for (let item of keyArr.values()) {
         if (item === fsName) {
           return {
             fileEnd: key,
-            userLanguage: true
-          };
+            userLanguage: true,
+          }
         }
       }
     }
   }
 
-  return false;
+  return false
 }
 
 // 项目使用特殊库/规则，导致文件语言跟注释形式不匹配 如：变量.blade.php与test.php的注释不同
 function specialLanguageFn(fsPath, config) {
-  config = config.configObj['language']; // 自定义语言项
-  const pathArr = fsPath.split('/');
-  const fileName = pathArr[pathArr.length - 1]; // 取/最后一位
-  Object.keys(config).forEach(item => {
+  config = config.configObj['language'] // 自定义语言项
+  const pathArr = fsPath.split('/')
+  const fileName = pathArr[pathArr.length - 1] // 取/最后一位
+  Object.keys(config).forEach((item) => {
     if (item.indexOf('.') !== -1) {
       // 限制key包含. fileName包含key fileName与key不等(变量.后缀.后缀)
       if (fileName.indexOf(item) !== -1 && fileName !== item) {
-        return item;
+        return item
       }
     }
-  });
+  })
 }
 
 // 修改时间格式
-Date.prototype.format = function() {
-  const config = vscode.workspace.getConfiguration('fileheader'); // 配置项
-  return moment(this)
-    .local()
-    .format(config.configObj.dateFormat);
-};
+Date.prototype.format = function () {
+  const config = vscode.workspace.getConfiguration('fileheader') // 配置项
+  return moment(this).local().format(config.configObj.dateFormat)
+}
 
 // 获取该文件的冒号
-const getColon = fileEnd => {
-  const config = vscode.workspace.getConfiguration('fileheader'); // 配置项默认值
-  let colon = config.configObj.colonObj[fileEnd]; // 冒号
+const getColon = (fileEnd) => {
+  const config = vscode.workspace.getConfiguration('fileheader') // 配置项默认值
+  let colon = config.configObj.colonObj[fileEnd] // 冒号
   // 文件没有设置 采用全局
   if (colon === undefined) {
-    colon = config.configObj.colon;
+    colon = config.configObj.colon
   }
-  return colon;
-};
+  return colon
+}
 
 /**
  * 切割特殊字符串生成空行
  * @param {string} tpl 生成的模板
  */
 const replaceSymbolStr = (tpl, fileEnd) => {
-  let sinceOut = tpl.indexOf('symbol_custom_string_obkoro1');
+  let sinceOut = tpl.indexOf('symbol_custom_string_obkoro1')
   if (sinceOut !== -1) {
-    const colon = getColon(fileEnd);
-    tpl = tpl.replace(`symbol_custom_string_obkoro1${colon}`, '');
+    const colon = getColon(fileEnd)
+    tpl = tpl.replace(`symbol_custom_string_obkoro1${colon}`, '')
   }
-  return tpl;
-};
+  return tpl
+}
 
 /**
  * 使用空格填充字符
  */
 const spaceStringFn = (oldStr, maxNum) => {
+  const config = vscode.workspace.getConfiguration('fileheader')
+  if (!config.configObj.wideSame) return oldStr // 不改变长度
   if (typeof maxNum !== 'number') {
     // 不为数字默认为13
-    maxNum = 13;
+    maxNum = 13
   }
-  let diffNum = maxNum - oldStr.length;
-  let spaceStr = ''.padStart(diffNum);
-  return `${oldStr}${spaceStr}`;
-};
+  let diffNum = maxNum - oldStr.length
+  let spaceStr = ''.padStart(diffNum)
+  return `${oldStr}${spaceStr}`
+}
 
 // 获取文件和项目的地址
 const getFileRelativeSite = () => {
-  const editor = vscode.editor || vscode.window.activeTextEditor; // 选中文件
-  const fsPath = editor._documentData._uri.fsPath; // 文件路径
-  let itemName = ''; // 项目名称
-  let itemPath = ''; // 项目路径
+  const editor = vscode.editor || vscode.window.activeTextEditor // 选中文件
+  const fsPath = editor._documentData._uri.fsPath // 文件路径
+  let itemName = '' // 项目名称
+  let itemPath = '' // 项目路径
   try {
-    itemPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    itemPath = vscode.workspace.workspaceFolders[0].uri.fsPath
     // path.sep window: \ mac: /
-    let itemNameArr = itemPath.split(path.sep);
-    itemName = itemNameArr[itemNameArr.length - 1]; // 取/最后一位
+    let itemNameArr = itemPath.split(path.sep)
+    itemName = itemNameArr[itemNameArr.length - 1] // 取/最后一位
   } catch (err) {
-    itemName = vscode.workspace.name;
-    itemPath = vscode.workspace.rootPath;
+    itemName = vscode.workspace.name
+    itemPath = vscode.workspace.rootPath
   }
-  let fileItemPath = fsPath.replace(itemPath, ''); // 相对地址
+  let fileItemPath = fsPath.replace(itemPath, '') // 相对地址
   return {
     fsPath, // 文件绝对地址
     itemPath, // 项目的绝对地址
     fileItemPath, // 文件相对地址
-    itemName // 项目名称
-  };
+    itemName, // 项目名称
+  }
+}
+
+// 自动添加是否匹配黑名单
+const isMatchProhibit = (fsPath) => {
+  const config = vscode.workspace.getConfiguration('fileheader'); // 配置项默认值
+  let match = false;
+  let prohibit = config.configObj.prohibitAutoAdd;
+  let fsName = fsPathFn(fsPath);
+  if (prohibit && prohibit.length > 0) {
+    match = prohibit.includes(fsName);
+  }
+  return match;
 };
 
 module.exports = {
@@ -217,5 +250,6 @@ module.exports = {
   replaceSymbolStr, // 切割特殊字符串生成空行
   getColon, // 获取该文件的冒号
   spaceStringFn, // 使用空格填充字符
-  getFileRelativeSite // 获取文件和项目的地址
-};
+  getFileRelativeSite, // 获取文件和项目的地址
+  isMatchProhibit // 自动添加是否匹配黑名单
+}
