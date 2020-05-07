affdfde993bf2b247246a617d6d8143938356e7b
OBKoro1 <1677593011@qq.com>
2020-05-07 14:07:49 +0800

    修复window下filePath报错的问题

--- src/logic/filePath.js
+++ src/logic/filePath.js
@@ -3,61 +3,65 @@
  * Date         : 2020-02-06 12:26:22
  * LastEditors  : OBKoro1
  * LastEditTime : 2020-02-06 13:23:18
- * FilePath     : /koro1FileHeader/src/logic/filePath.js
+ * FilePath     : /koro1FileHeader/src/logic/filePath.js23333
  * Description  : 文件路径相关
  * https://github.com/OBKoro1
  */
-const vscode = require('vscode');
-const util = require('../utile/util');
-const path = require('path');
-const logicUtil = require('../utile/logicUtil');
-const languageDifferent = require('../languageOutPut/languageDifferent');
+const vscode = require('vscode')
+const util = require('../utile/util')
+const path = require('path')
+const logicUtil = require('../utile/logicUtil')
+const languageDifferent = require('../languageOutPut/languageDifferent')
 
-const createFilePath = FilePath => {
-  const config = vscode.workspace.getConfiguration('fileheader');
-  let { itemName, fileItemPath } = util.getFileRelativeSite();
-  let res = `${path.sep}${itemName}${fileItemPath}`; // 拼接项目名称和相对于项目的路径
+const createFilePath = (FilePath) => {
+  const config = vscode.workspace.getConfiguration('fileheader')
+  let { itemName, fileItemPath } = util.getFileRelativeSite()
+  let res = `${path.sep}${itemName}${fileItemPath}` // 拼接项目名称和相对于项目的路径
   if (FilePath === 'no item name') {
-    res = `${fileItemPath}`;
+    res = `${fileItemPath}`
   }
   if (config.configObj.filePathColon !== '路径分隔符替换') {
     // path.sep window: \ mac: /
-    const reg = new RegExp(path.sep, 'y');
-    res = res.replace(reg, config.configObj.filePathColon);
+    let pathStr = '/' // mac
+    if (path.sep !== pathStr) {
+      pathStr = '\\\\' // window \在RegExp中需要再转义一次
+    }
+    const reg = new RegExp(pathStr, 'g')
+    res = res.replace(reg, config.configObj.filePathColon)
   }
-  return res;
-};
+  return res
+}
 
 // 模拟生成中间部分
 const mockCreateMiddle = (FilePath, fileEnd) => {
-  FilePath = createFilePath(FilePath); // 生成FilePath
+  FilePath = createFilePath(FilePath) // 生成FilePath
   let data = {
-    FilePath
-  };
-  data = logicUtil.sameLengthFn(data); // FilePath长度
+    FilePath,
+  }
+  data = logicUtil.sameLengthFn(data) // FilePath长度
   // 只生成路径
-  let result = '';
-  Object.keys(data).forEach(key => {
+  let result = ''
+  Object.keys(data).forEach((key) => {
     const obj = {
       fileEnd,
       type: 'topMiddle',
       key,
-      value: data[key]
-    };
-    let res = new languageDifferent.tplJudge(obj).res;
-    res = res.split('\r\n');
+      value: data[key],
+    }
+    let res = new languageDifferent.tplJudge(obj).res
+    res = res.split('\r\n')
     if (res[1] !== undefined) {
       // 如果换行后增加了空格等 则拼接在前面
-      result = `${res[1]}${res[0]}`;
+      result = `${res[1]}${res[0]}`
     } else {
-      // 没有空格 
-      result = `${res[0]}`;
+      // 没有空格
+      result = `${res[0]}`
     }
-  });
-  return result;
-};
+  })
+  return result
+}
 
 module.exports = {
   createFilePath,
-  mockCreateMiddle
-};
+  mockCreateMiddle,
+}
