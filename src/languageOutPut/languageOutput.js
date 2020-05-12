754077240d46ea635d9298d263b7f4564fcb5533
OBKoro1 <1677593011@qq.com>
2020-05-12 20:25:17 +0800

    format

--- src/languageOutPut/languageOutput.js
+++ src/languageOutPut/languageOutput.js
@@ -6,25 +6,25 @@
  * LastEditTime : 2020-02-05 12:37:07
  * @Description: 不同语言的逻辑
  */
-const languageDifferent = require('./languageDifferent');
-const constFile = require('../utile/CONST');
-const util = require('../utile/util');
+const languageDifferent = require('./languageDifferent')
+const constFile = require('../utile/CONST')
+const util = require('../utile/util')
 
 // 头部注释中间部分生成
 const middleTpl = (data, fileEnd, config) => {
-  let str = '';
-  Object.keys(data).forEach(key => {
+  let str = ''
+  Object.keys(data).forEach((key) => {
     const obj = {
       fileEnd,
       type: 'topMiddle',
       key,
-      value: newlineAddAnnotationFn(data[key], fileEnd, config)
-    };
-    const res = new languageDifferent.tplJudge(obj).res;
-    str = str + res;
-  });
-  return str;
-};
+      value: newlineAddAnnotationFn(data[key], fileEnd, config),
+    }
+    const res = new languageDifferent.tplJudge(obj).res
+    str = str + res
+  })
+  return str
+}
 
 /**
  * @description: 文件头部注释生成
@@ -33,15 +33,15 @@ const middleTpl = (data, fileEnd, config) => {
  * @return: 字符串
  */
 const headNotes = (data, fileEnd, config) => {
-  let str = middleTpl(data, fileEnd, config);
+  let str = middleTpl(data, fileEnd, config)
   // 头部 中间模板 尾部合并
   const obj = {
     fileEnd,
     type: 'topHeadEnd',
-    str
-  };
-  return new languageDifferent.tplJudge(obj).res;
-};
+    str,
+  }
+  return new languageDifferent.tplJudge(obj).res
+}
 
 class functionTplStr {
   /**
@@ -54,21 +54,21 @@ class functionTplStr {
    * @return: 函数注释的模板字符串
    */
   constructor(data, fileEnd, lineSpace, nextLine, frontStr) {
-    this.fileEnd = fileEnd;
-    this.nextLine = nextLine;
-    this.frontStr = frontStr;
-    this.str = ''.padStart(lineSpace); // 生成指定长度的字符串
-    this.strContent = '';
-    this.data = data;
+    this.fileEnd = fileEnd
+    this.nextLine = nextLine
+    this.frontStr = frontStr
+    this.str = ''.padStart(lineSpace) // 生成指定长度的字符串
+    this.strContent = ''
+    this.data = data
   }
   // 生成函数注释模板
   generate() {
     // 生成中间模板
-    Object.keys(this.data).forEach(key => {
-      this.strContent += this.paramStr(key);
-    });
-    this.tpl = this.mergeStr();
-    return this.replaceSymbolStr(this.tpl);
+    Object.keys(this.data).forEach((key) => {
+      this.strContent += this.paramStr(key)
+    })
+    this.tpl = this.mergeStr()
+    return this.replaceSymbolStr(this.tpl)
   }
 
   /**
@@ -79,41 +79,41 @@ class functionTplStr {
       fileEnd: this.fileEnd,
       str: this.str,
       key,
-      value: this.data[key]
-    };
+      value: this.data[key],
+    }
     // 注释是参数的话 多加一个参数的属性
     if (key === 'param') {
-      obj.type = 'fnMiddle_param';
-      obj.typeVal = '{type}';
+      obj.type = 'fnMiddle_param'
+      obj.typeVal = '{type}'
     } else {
-      obj.type = 'fnMiddle_key';
+      obj.type = 'fnMiddle_key'
     }
-    return new languageDifferent.tplJudge(obj).res;
+    return new languageDifferent.tplJudge(obj).res
   }
   mergeStr() {
     const obj = {
       fileEnd: this.fileEnd,
       frontStr: this.frontStr,
       strContent: this.strContent,
-      str: this.str
-    };
+      str: this.str,
+    }
     if (this.nextLine === undefined) {
       // 当前行不为空
-      obj.type = 'topHeadEnd_nextLineNo';
+      obj.type = 'topHeadEnd_nextLineNo'
     } else {
       // 当前行为空
-      obj.type = 'topHeadEnd_nextLineYes';
+      obj.type = 'topHeadEnd_nextLineYes'
     }
-    return new languageDifferent.tplJudge(obj).res;
+    return new languageDifferent.tplJudge(obj).res
   }
   // 切割特殊字符串生成空行
   replaceSymbolStr(tpl) {
-    let sinceOut = tpl.indexOf('symbol_custom_string_obkoro1');
+    let sinceOut = tpl.indexOf('symbol_custom_string_obkoro1')
     if (sinceOut !== -1) {
-      const colon = util.getColon(this.fileEnd);
-      tpl = tpl.replace(`symbol_custom_string_obkoro1${colon}`, '');
+      const colon = util.getColon(this.fileEnd)
+      tpl = tpl.replace(`symbol_custom_string_obkoro1${colon}`, '')
     }
-    return tpl;
+    return tpl
   }
 }
 
@@ -123,68 +123,68 @@ class functionTplStr {
  */
 class changeFont {
   constructor(fileEnd) {
-    this.fileEnd = fileEnd;
+    this.fileEnd = fileEnd
   }
   // 输出注释开头：用以判断是否进入注释
   star() {
     const obj = {
       fileEnd: this.fileEnd,
-      type: 'annotationStarts'
-    };
-    return new languageDifferent.tplJudge(obj).res;
+      type: 'annotationStarts',
+    }
+    return new languageDifferent.tplJudge(obj).res
   }
   // 最后编辑人
   LastEditorsStr(LastEditors) {
     const obj = {
       fileEnd: this.fileEnd,
       type: 'LastEditorsStr',
-      LastEditors
-    };
-    return new languageDifferent.tplJudge(obj).res;
+      LastEditors,
+    }
+    return new languageDifferent.tplJudge(obj).res
   }
   // 最后编辑时间
   lastTimeStr() {
     const obj = {
       fileEnd: this.fileEnd,
-      type: 'lastTimeStr'
-    };
-    return new languageDifferent.tplJudge(obj).res;
+      type: 'lastTimeStr',
+    }
+    return new languageDifferent.tplJudge(obj).res
   }
 }
 
 // 换行符加注释符号
 function newlineAddAnnotationFn(value, fileEnd, config) {
   if (config.configObj.switch.newlineAddAnnotation) {
-    let middle = null;
+    let middle = null
     // 匹配用户定义语言符号
     if (fileEnd.userLanguage) {
-      middle = config.configObj.language[fileEnd.fileEnd].middle;
+      middle = config.configObj.language[fileEnd.fileEnd].middle
     } else if (fileEnd !== '匹配不到_默认注释') {
       // 匹配插件的符号
-      middle = constFile.middleAnnotation[fileEnd];
+      middle = constFile.annotationSymbol[fileEnd].middle
     } else if (config.configObj.annotationStr.use) {
       // 调用用户设置的默认注释符号
-      middle = config.configObj.language.middle;
+      middle = config.configObj.language.middle
     } else {
       // 插件默认设置
-      middle = constFile.middleAnnotation.javascript;
+      middle = constFile.annotationSymbol.javascript.middle
     }
     if (middle !== null) {
       // \n 换行
       // \r 回车
       // \r\n Windows系统里面，每行结尾是“<回车><换行>”
-      value = value.replace(/\r\n/g, `\obkoro1\obkoro1${middle}`); // 转化为特殊字符 不影响下面的替换
-      value = value.replace(/\n/g, `\n${middle}`);
-      value = value.replace(/\r/g, `\r${middle}`);
-      value = value.replace(/\obkoro1\obkoro1/g, `\r\n`); // 转化回来
+      value = value.replace(/\r\n/g, `\obkoro1\obkoro1${middle}`) // 转化为特殊字符 不影响下面的替换
+      value = value.replace(/\n/g, `\n${middle}`)
+      value = value.replace(/\r/g, `\r${middle}`)
+      value = value.replace(/\obkoro1\obkoro1/g, `\r\n`) // 转化回来
     }
   }
-  return value;
+  return value
 }
 
 module.exports = {
   headNotes,
   functionTplStr,
   changeFont,
-  middleTpl
-};
+  middleTpl,
+}
