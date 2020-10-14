4a5fbe5d5ca66d0b347c9c99deaafe957fe5170c
OBKoro1 <1677593011@qq.com>
2020-10-14 19:29:33 +0800

    add: 添加go语言的参数解析

---
+++ src/function-params/function-go.js
@@ -0,0 +1,67 @@
+/*
+ * Author       : OBKoro1
+ * CreateDate   : 2020-09-07 15:47:40
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2020-10-11 18:40:08
+ * FilePath     : \koro1FileHeader\src\function-params\function-go.js
+ * Description  : java语言获取函数参数
+ */
+
+class GetParams {
+  init(lineProperty) {
+    this.text = lineProperty._text // 代码
+    this.match = false // 是否匹配到参数
+    this.res = ''
+    this.matchProcess()
+  }
+
+  // 匹配流程
+  matchProcess() {
+    const processArr = ['matchFunction']
+    let params = ''
+    for (const item of processArr.values()) {
+      const match = this[item]()
+      if (match) {
+        // let methodName = res[1] // 方法名
+        params = match[2]
+        break
+      }
+    }
+    // 匹配参数
+    this.parsing(params)
+  }
+
+  // 匹配方法声明的参数
+  matchFunction() {
+    // 匹配单词func 可能有空格 可能有函数名 可能有空格 匹配括号 匹配括号内的一切
+    const reg = /\bfunc\b\s*(\w*)\s*\((.*)\)/
+    return reg.exec(this.text)
+  }
+
+  parsing(params) {
+    let res
+    let paramsArr = [] // 参数列表
+    // 可能的空格 匹配参数 匹配可能的参数类型 遇到逗号停下来
+      const reg = /\s*([A-Za-z_]\w*)(\s+[^,]*)?[^,]*/g
+    // 捕获函数参数
+    while ((res = reg.exec(params))) {
+      if (!res) break
+      if (res[2] !== undefined) {
+        res[2] = res[2].replace(/\s+/g, '')
+      } else {
+        res[2] = '*'
+      }
+      const obj = {
+        type: res[1],
+        param: res[2],
+      }
+      paramsArr.push(obj)
+    }
+    this.res = paramsArr
+    if (paramsArr.length !== 0) {
+      this.match = true
+    }
+  }
+}
+
+module.exports = new GetParams()
