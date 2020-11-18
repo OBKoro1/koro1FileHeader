7f40f239642578a4d81eb5c4ab4407eb3ef88962
OBKoro1 <1677593011@qq.com>
2020-11-18 16:41:43 +0800

    chore: init

--- deploy.sh
+++ deploy.sh
@@ -17,3 +17,13 @@ vsce package --yarn # 解决下文报错
 # vscodium发布插件
 
 # npx ovsx publish file -p token
+
+# tag
+# https://git-scm.com/book/zh/v2/Git-%E5%9F%BA%E7%A1%80-%E6%89%93%E6%A0%87%E7%AD%BE
+
+# git tag -a 'v0.0.12' -m 'tag信息'
+# git push origin --tags
+# git tag
+
+# git tag -l | xargs git tag -d #删除所有本地分支
+# git fetch origin --prune #从远程拉取所有信息
