a8b82147f2184ad6bf47d1becf3c8fc72eae92c1
OBKoro1 <1677593011@qq.com>
2020-12-24 15:45:14 +0800

    fix: 修复`go`函数注释参数提取时，提取函数签名的问题

--- src/function-params/function-go.js
+++ src/function-params/function-go.js
@@ -1,9 +1,9 @@
 /*
  * Author       : OBKoro1
  * CreateDate   : 2020-09-07 15:47:40
- * ,@LastEditors  : OBKoro1
- * ,@LastEditTime : 2020-10-18 16:30:59
- * ,@FilePath     : \koro1FileHeader\src\function-params\function-go.js
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2020-12-24 15:44:06
+ * File         : \koro1FileHeader\src\function-params\function-go.js
  * Description  : java语言获取函数参数
  */
 
@@ -17,13 +17,18 @@ class GetParams {
 
   // 匹配流程
   matchProcess() {
-    const processArr = ['matchFunction']
+    const matchObj = {
+      matchFunction: 2,
+      matchFuncNoName: 1,
+    }
     let params = ''
-    for (const item of processArr.values()) {
+    const keyArr = Object.keys(matchObj)
+    for (const item of keyArr.values()) {
       const match = this[item]()
       if (match) {
         // let methodName = res[1] // 方法名
-        params = match[2]
+        const index = matchObj[item]
+        params = match[index]
         break
       }
     }
@@ -34,7 +39,12 @@ class GetParams {
   // 匹配方法声明的参数
   matchFunction() {
     // 匹配单词func 可能有空格 可能有函数名 可能有空格 匹配括号 匹配括号内的一切
-    const reg = /\bfunc\b\s*(\w*)\s*\((.*?)\)/
+    const reg = /\bfunc\b.*(\w+)\s*\((.*?)\)/
+    return reg.exec(this.text)
+  }
+  // 匹配匿名函数  var Add = func(a, b int) int {
+  matchFuncNoName() {
+    const reg = /\bfunc\b\s*\((.*?)\)/
     return reg.exec(this.text)
   }
 
@@ -42,7 +52,7 @@ class GetParams {
     let res
     let paramsArr = [] // 参数列表
     // 可能的空格 匹配参数 匹配可能的参数类型 遇到逗号停下来
-      const reg = /\s*([A-Za-z_]\w*)(\s+[^,]*)?[^,]*/g
+    const reg = /\s*([A-Za-z_]\w*)(\s+[^,]*)?[^,]*/g
     // 捕获函数参数
     while ((res = reg.exec(params))) {
       if (!res) break
