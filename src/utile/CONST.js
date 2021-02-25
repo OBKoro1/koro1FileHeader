dbe5ed3a8880c38f49c5ace42d57eb7bd7c8e544
OBKoro1 <1677593011@qq.com>
2021-02-25 16:35:22 +0800

    feat: 常量位置调整

--- src/utile/CONST.js
+++ src/utile/CONST.js
@@ -9,6 +9,31 @@
  * https://github.com/OBKoro1
  */
 
+// 可以修改的
+let context = '' // 插件上下文
+let autoAddFiles = [] // 自动添加的文件
+let errPath = '' // 错误地址 
+
+
+// 不可修改的
+
+// 检测头部注释的行数 需要检测注释图案
+const lineNum = 15 
+// pre commit 执行node的字符串
+const handleNodeString =
+  'node ./.git/hooks/fileHeader-checkChange.js # koroFileHeader的commit hooks，判断文件只改变时间，就不进行操作'
+// 特殊字符串
+const specialString = `custom_string_obkoro`
+
+// 自定义模板字符串
+const customStringConst = 'symbol_custom_string_obkoro'
+
+// 时间字段
+const customStringTime = 'symbol_custom_string_obkoro10000'
+
+// 版本字段
+const customStringCopyRight = 'symbol_custom_string_obkoro10001'
+
 //  不同语言的注释符号
 const annotationSymbol = {
   javascript: {
@@ -43,32 +68,16 @@ const annotationSymbol = {
   },
 }
 
-// 插件上下文
-let context = ''
-let autoAddFiles = [] // 自动添加的文件
-const lineNum = 15 // 检测头部注释的行数 需要检测注释图案
 
-// pre commit 执行node的字符串
-const handleNodeString =
-  'node ./.git/hooks/fileHeader-checkChange.js # koroFileHeader的commit hooks，判断文件只改变时间，就不进行操作'
 
-// 特殊字符串
-const specialString = `custom_string_obkoro`
 
-// 自定义模板字符串
-const customStringConst = 'symbol_custom_string_obkoro'
-
-// 时间字段
-const customStringTime = 'symbol_custom_string_obkoro10000'
-
-// 版本字段
-const customStringCopyRight = 'symbol_custom_string_obkoro10001'
 
 module.exports = {
   lineNum,
   autoAddFiles,
   annotationSymbol,
   context,
+  errPath,
   customStringConst,
   customStringTime,
   customStringCopyRight,
