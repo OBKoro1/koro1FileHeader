6c7e094f6556939929b540a949fd5a72e5927426
OBKoro1 <1677593011@qq.com>
2023-01-19 21:17:42 +0800

    feat: update

--- test/test-run/function-params/c++-function.cpp
+++ test/test-run/function-params/c++-function.cpp
@@ -2,9 +2,9 @@
  * Author       : OBKoro1
  * CreateDate   : 2020-08-16 17:20:35
  * LastEditors  : OBKoro1
- * LastEditTime : 2021-11-05 11:38:37
- * FilePath     \fileHead\function-params\c++-function.cpp
- * Description  : 
+ * LastEditTime : 2023-01-13 21:51:17
+ * FilePath     : /function-params/c++-function.cpp
+ * Description  :
  * 2020-08-16 17:20:35
  * 可以输入预定的版权声明、个性签名、空行等
  * Copyright 2021 OBKoro1
@@ -14,39 +14,38 @@
  */
 
 /**
- * @description: 
+ * @description:
  * @param {int} num1
  * @param {int} num2
  * @return {*}
  */
-int max(int num1, int num2) 
+int max(int num1, int num2)
 {
    // 局部变量声明
    int result;
- 
+
    if (num1 > num2)
       result = num1;
    else
       result = num2;
- 
-   return result; 
+
+   return result;
 }
 
 /***
- * description: 
+ * description:
  * param {int} a
  * param {int} b
  * return {*}
  **/
-int sum(int a, int b=20)
+int sum(int a, int b = 20)
 {
-  int result;
- 
-  result = a + b;
-  
-  return (result);
-}
+   int result;
 
+   result = a + b;
+
+   return (result);
+}
 
 /**
  * @description
@@ -54,46 +53,55 @@ int sum(int a, int b=20)
  * @return {*}
  */
 /**
- * description: 
+ * description:
  * param hWin [WM_HWIN]
  * return [type]
  */
 /**
- * description: 
+ * description:
  * param hWin [WM_HWIN]
  * return [type]
  */
-static void createKeyInit(WM_HWIN hWin){
-
+static void createKeyInit(WM_HWIN hWin)
+{
 }
 
 /**
- * description: 
+ * description:
  * param dataStr [vector&]
  * param begNumber [int]
  * param addNumber [int]
  * param offsetZ [double]
  * return [type]
  */
-void addOH(const vector<tuple<double, double, double> >& data, vector& dataStr, int begNumber, int addNumber, double offsetZ, vector<bool>& book){}
+void addOH(const vector<tuple<double, double, double>> &data, vector &dataStr, int begNumber, int addNumber, double offsetZ, vector<bool> &book) {}
 
 /**
- * description: 
+ * description:
  * param temp [string&]
  * return [type]
  */
 /**
- * description: 
+ * description:
  * param temp [std::string&]
  * return [type]
  */
-void test(const std::string& temp) {
+void test(const std::string &temp)
+{
 }
 
 /**
- * description: 
+ * description:
  * param temp [std::string*]
  * return [type]
  */
-void test2(const std::string* temp) {
+void test2(const std::string *temp)
+{
 }
+
+/**
+ * @description:
+ * @param {int} wave_type
+ * @return {type}
+ */
+void GenerateWave(int wave_type)
--- test/test-run/function-params/c-function.h
+++ test/test-run/function-params/c-function.h
@@ -1,1 +1,16 @@
 // cpp
+
+/**
+ * @description:
+ * @return {type}
+ */
+/**
+ * @description:
+ * @param {string} &host_name
+ * @param {string} &user_name
+ * @param {string} &email_name
+ * @return {type}
+ */
+handle vm_con(const std::string &host_name,
+              const std::string &user_name,
+              const std::string &email_name);
